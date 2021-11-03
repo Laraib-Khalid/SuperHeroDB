e60ae04df004c731ecf81ee0290209ac8459a9a7
HP <HP@DESKTOP-D6S2PMP>
2021-11-04 00:02:42 +0500

    Add project files.

---
+++ SuperHeroDB/Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroDB.Server.Controllers
+{
+    public class SuperHeroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
