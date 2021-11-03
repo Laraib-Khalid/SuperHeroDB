96d2dc8b78d2bca844ab41e75a365b9d3ae38d8f
Laraib Khaild <laraibkhalid7861@gmail.com>
2021-11-04 00:26:01 +0500

    Add Controller

--- SuperHeroDB/Server/Controllers/SuperHeroController.cs
+++ SuperHeroDB/Server/Controllers/SuperHeroController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroDB.Shared;
 
 namespace SuperHeroDB.Server.Controllers
 {
-    public class SuperHeroController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
     {
-        public IActionResult Index()
+        static List<Comic> comics = new List<Comic> 
+        { 
+            new Comic{ Name="Marvel"},
+            new Comic{ Name="DC"}
+        };
+        List<SuperHero> heroes = new List<SuperHero>
         {
-            return View();
+            new SuperHero {FirstName = "Peter", LastName ="Parker", HeroName="Spiderman", Comic= comics[0]},
+            new SuperHero {FirstName = "Bruce", LastName ="Wynce", HeroName="Batman", Comic= comics[1]}
+        };
+        public async Task<IActionResult> GetSuperHeroes()
+        {
+            return Ok(heroes);
         }
     }
 }
--- SuperHeroDB/Shared/Comic.cs
+++ SuperHeroDB/Shared/Comic.cs
@@ -8,6 +8,7 @@ namespace SuperHeroDB.Shared
 {
     public class Comic
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
